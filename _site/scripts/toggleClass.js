26136690d091ba34e3d62ddac101d2750f836f44
Tyler Hancock <tyler.hancock28@gmail.com>
2019-01-07 10:41:39 -0500

    + author

---
+++ _site/scripts/toggleClass.js
@@ -0,0 +1,6 @@
+//$(document).ready( function(){
+//    $('.isi').click( function() {
+//        var toggleHeight = $(".isi").height() == 600 ? "70px" : "600px";
+//        $('.isi').animate({ height: toggleHeight });
+//    });
+//});
