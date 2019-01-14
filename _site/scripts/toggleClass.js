2aa953e0f414842b370a51d59ad0b5e6ced4fa1e
Tyler Hancock <tyler.hancock28@gmail.com>
2019-01-13 22:20:42 -0500

    color
    + 'X' rotate
    + increase div height

--- _site/scripts/toggleClass.js
+++ _site/scripts/toggleClass.js
@@ -1,6 +1,15 @@
-//$(document).ready( function(){
-//    $('.isi').click( function() {
-//        var toggleHeight = $(".isi").height() == 600 ? "70px" : "600px";
-//        $('.isi').animate({ height: toggleHeight });
-//    });
-//});
+$(document).ready( function(){
+    $('.color .isi').click( function() {
+        var toggleHeight = $(".color .isi").height() == 600 ? "70px" : "600px";
+        $('.color .isi').animate({ height: toggleHeight });
+    });
+});
+
+
+$(function(){
+  $(".color").click(function(){
+    /*//Animação com loop infinito (até clicar)
+    $("#d2").toggleClass("rotating");*/
+    $(".color .plus").toggleClass("spinEffect");
+  });
+});
